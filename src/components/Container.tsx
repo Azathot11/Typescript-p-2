1962874531804163474144fcee5a9a2370fa2eac
Azathot11 <azathotkamdoum@gmail.com>
2023-11-21 16:35:11 +0100

    add

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,9 +1,19 @@
-import {ComponentPropsWithoutRef, type ElementType, type FC, type ReactNode} from "react";
+import {ComponentPropsWithoutRef, type ElementType,  type ReactNode} from "react";
 
-type ContainerProps<T extends  ElementType> = {
+type ContainerProps <T extends  ElementType> = {
     as:T
     children:ReactNode
 } & ComponentPropsWithoutRef<T>
+
+export default function Container({as,children,}:ContainerProps<'div'>) {
+    const Component = as || 'div'
+
+  return (
+         <Component>
+             {children}
+         </Component>
+     );
+}
 
 // const Container:FC<ContainerProps<C>> = ({as,children}:<C>) => {
 //     const Component = as || 'div'
